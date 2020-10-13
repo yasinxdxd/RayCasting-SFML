998b28e7eec13cecdd91f4ea12b348bf9f144513
yasinxdxd <42904721+yasinxdxd@users.noreply.github.com>
2020-10-13 17:47:00 +0300

    Ray casting is ready
    ras casting is working. But all walls should managing from one array.

---
+++ include/Line.h
@@ -0,0 +1,33 @@
+#ifndef LINE_H
+#define LINE_H
+#include <iostream>
+#include <cmath>
+#include <cstdint>
+#include <cstring>
+#include <SFML/Graphics.hpp>
+#define WIDTH 1920
+#define HEIGHT 1080
+#define PI 3.14159265359
+
+class Line
+{
+public:
+    Line(float _x1, float _y1, float _x2, float _y2);
+    Line();
+    virtual ~Line();
+    void show(sf::RenderWindow *window);
+    void setPos(float _x1, float _y1, float _x2, float _y2);
+    sf::Vertex getPoint1() {return point1;}
+    sf::Vertex getPoint2() {return point2;}
+private:
+    sf::VertexArray L;
+    sf::Vertex point1;
+    sf::Vertex point2;
+    bool isIntersects;
+
+friend class Player;
+friend class Game;
+
+};
+
+#endif // LINE_H
